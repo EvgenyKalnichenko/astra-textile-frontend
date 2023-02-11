27d54e9db7e593d988a374df6e09aca68b3113d2
kalni <kalnichenko.1996@mail.ru>
2023-02-11 12:36:57 +0300

    Блок "Отзывы"

--- src/js/modules/swiperInit.js
+++ src/js/modules/swiperInit.js
@@ -1,5 +1,6 @@
 import Swiper, {Navigation, Pagination} from "swiper";
 import {initIntersectionObserver} from "./initIntersectionObserver";
+import {Breakpoints} from "./breakpoints";
 
 export function swiperInit () {
     document.querySelectorAll('.main-slider:not(.swiper-init)').forEach((el) => {
@@ -27,7 +28,7 @@ export function swiperInit () {
                     slidesPerView: 1,
                 },
                 // when window width is >= 640px
-                1024: {
+                1023: {
                     slidesPerView: 2,
                 }
             }
@@ -49,11 +50,49 @@ export function swiperInit () {
                 pagination: {
                     el: el.querySelector('.swiper-pagination--card-product'),
                     type: 'bullets',
-                    // clickable: true
                 }
             })
         },
         document.querySelectorAll('.card-product__images:not(.init)')
     )
 
+    initIntersectionObserver(
+        (item) => {
+            const el = item.target;
+            el.classList.add('init')
+            new Swiper (el.querySelector('.swiper'), {
+                modules: [Pagination, Navigation],
+                loop: false,
+                watchOverflow: true,
+                speed: 200,
+                slideClass: 'swiper-slide',
+                navigation: {
+                    nextEl: el.querySelector('.swiper-arrow__item--next'),
+                    prevEl: el.querySelector('.swiper-arrow__item--prev'),
+                },
+                breakpoints: {
+                    // when window width is >= 320px
+                    0: {
+                        slidesPerView: 1,
+                        spaceBetween: 15,
+                        pagination: {
+                            el: el.querySelector('.swiper-pagination'),
+                            type: 'bullets',
+                            clickable: true
+                        },
+                    },
+                    // when window width is >= 640px
+                    767: {
+                        slidesPerView: 2,
+                        spaceBetween: 15,
+                    },
+                    1279: {
+                        spaceBetween: 30,
+                        slidesPerView: 3,
+                    }
+                }
+            })
+        },
+        document.querySelectorAll('.slider-reviews:not(.init)')
+    )
 }
