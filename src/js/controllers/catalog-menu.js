d34c3ac5b23d3b4fbfeaaaa65bb88858bf47b573
kalni <kalnichenko.1996@mail.ru>
2023-02-13 10:14:22 +0300

    мобильное меню + меню каталога

--- src/js/controllers/catalog-menu.js
+++ src/js/controllers/catalog-menu.js
@@ -1,4 +1,5 @@
 import {modal} from "../modules/modal";
+import {swiperInit} from "../modules/swiperInit";
 
 $(document).on('mouseenter', '[data-menu-open]', openMenu)
 
@@ -20,12 +21,30 @@ function openMenu() {
     closeMenu()
     const $this = $(this)
     const name = $this.attr('data-menu-open')
-    // $this.addClass('is-active')
+    $this.addClass('is-active')
     console.log('name', name)
     modal.open(name)
 }
 
 function closeMenu() {
     $('.menu-catalog__second.is-active').removeClass('is-active')
+
     $('[data-menu-open]').removeClass('is-active')
 }
+
+$(document).on('modal-open', (params, name) => {
+    if(name === 'menu-catalog') {
+        $('.header__menu').addClass('is-active')
+    }
+})
+
+$(document).on('modal-close', (params, name) => {
+    if(name === 'menu-catalog') {
+        if(!$('.menu-mobile').hasClass('is-active')) $('.header__menu').removeClass('is-active')
+    }
+})
+
+$(document).on('click', '.menu-catalog__back', function () {
+    const name = $(this).attr('data-modal-close')
+    $(`[data-menu-open="${name}"]`).removeClass('is-active')
+})
